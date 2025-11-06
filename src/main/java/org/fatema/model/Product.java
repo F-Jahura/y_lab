5b4d0b2da24fb449c3189cc4d122f23a98723d29
F-Jahura <f_jahura@yahoo.com>
2025-11-06 22:54:52 +0300

    added y_lab project

---
+++ src/main/java/org/fatema/model/Product.java
@@ -0,0 +1,70 @@
+package org.fatema.model;
+
+public class Product {
+    private Long id;
+    private String title;
+    private int price;
+    private String category;
+    private String brand;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", brand='" + brand + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
